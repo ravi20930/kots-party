03b5387a1bdf0db511dd085ec7049f03c71b7748
ravi <ravi20930@gmail.com>
2024-12-11 21:53:06 +0530

    fix: typescript error in Providers component

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
 
 export default function Providers({
@@ -7,7 +8,7 @@ export default function Providers({
   session
 }: {
   children: React.ReactNode
-  session: any
+  session: Session | null
 }) {
   return <SessionProvider session={session}>{children}</SessionProvider>
 }
